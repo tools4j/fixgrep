44168d6399e77d3e639195fc32a4ac03944092bc
ben <bjwarner@gmail.com>
2018-09-03 07:07:04 +0100

    Added a FixParser test

--- shared/src/main/kotlin/org/tools4j/fix/spec/FixSpecParser.kt
+++ shared/src/main/kotlin/org/tools4j/fix/spec/FixSpecParser.kt
@@ -21,7 +21,7 @@ class FixSpecParser(private val specInputStream: InputStream) {
         @JvmStatic
         fun main(args: Array<String>){
             val before = System.currentTimeMillis()
-            val fixSpec = FixSpecParser("FIX50SP2.xml").parseSpec();
+            val fixSpec = FixSpecParser("FIX50SP2.xml").parseSpec()
             val after = System.currentTimeMillis()
             //println(after - before)
         }
@@ -42,13 +42,13 @@ class FixSpecParser(private val specInputStream: InputStream) {
             val localFieldsByName = LinkedHashMap<String, FixSpecDefinition.Field>()
             for (i in 0 until localFieldList.length) {
                 val fieldNode: Node = localFieldList.item(i)
-                if (fieldNode.getNodeType() != Node.ELEMENT_NODE) continue
+                if (fieldNode.nodeType != Node.ELEMENT_NODE) continue
                 val fieldElement = fieldNode as Element
 
                 val enums = LinkedHashSet<FixSpecDefinition.FieldEnum>()
                 for (j in 0 until fieldElement.childNodes.length) {
                     val enumNode: Node = fieldElement.childNodes.item(j)
-                    if (enumNode.getNodeType() != Node.ELEMENT_NODE) continue
+                    if (enumNode.nodeType != Node.ELEMENT_NODE) continue
                     val enumElement = enumNode as Element
                     if (enumElement.tagName != "value") continue
                     enums.add(FixSpecDefinition.FieldEnum(enumElement.getAttribute("enum"), enumElement.getAttribute("description")))
@@ -61,7 +61,7 @@ class FixSpecParser(private val specInputStream: InputStream) {
                     throw e
                 }
             }
-            return localFieldsByName;
+            return localFieldsByName
         }
         fieldsByName.putAll(extractFields(fieldList))
 
@@ -84,7 +84,7 @@ class FixSpecParser(private val specInputStream: InputStream) {
         val rawComponentsByName = LinkedHashMap<String, RawComponent>()
         for(i in 0 until componentsList.length){
             val componentNode: Node = componentsList.item(i)
-            if (componentNode.getNodeType() != Node.ELEMENT_NODE) continue
+            if (componentNode.nodeType != Node.ELEMENT_NODE) continue
             val componentElement = componentNode as Element
             val constituents = extractConstituents(componentElement, fieldsByName)
             val componentName = componentElement.getAttribute("name")
@@ -99,7 +99,7 @@ class FixSpecParser(private val specInputStream: InputStream) {
         val rawMessagesByName = LinkedHashMap<String, RawMessage>()
         for(i in 0 until messagesList.length){
             val messageNode: Node = messagesList.item(i)
-            if (messageNode.getNodeType() != Node.ELEMENT_NODE) continue
+            if (messageNode.nodeType != Node.ELEMENT_NODE) continue
             val messageElement = messageNode as Element
             val constituents = extractConstituents(messageElement, fieldsByName)
             rawMessagesByName.put(messageElement.getAttribute("name"), RawMessage(messageElement.getAttribute("name"), messageElement.getAttribute("msgtype"), constituents))
@@ -122,7 +122,7 @@ class FixSpecParser(private val specInputStream: InputStream) {
         val messagesByName = LinkedHashMap<String, FixSpecDefinition.Message>()
         val componentsByName = LinkedHashMap<String, FixSpecDefinition.Component>()
 
-        public fun expand(): FixSpecDefinition {
+        fun expand(): FixSpecDefinition {
             for(rawMessage in rawMessagesByName.values){
                 messagesByName.put(rawMessage.name, FixSpecDefinition.Message(rawMessage.name, rawMessage.msgType, expandConstituents(rawMessage.constituents)))
             }
@@ -165,7 +165,7 @@ class FixSpecParser(private val specInputStream: InputStream) {
         val constituents = ArrayList<Any>()
         for (j in 0 until parentElement.childNodes.length) {
             val childNode: Node = parentElement.childNodes.item(j)
-            if (childNode.getNodeType() == Node.ELEMENT_NODE) {
+            if (childNode.nodeType == Node.ELEMENT_NODE) {
                 val childElement = childNode as Element
                 if (childElement.tagName == "field") {
                     constituents.add(fieldsMap[childElement.getAttribute("name")!!]!!)
---
+++ shared/src/test/groovy/org/tools4j/fix/spec/FixSpecParserTest.groovy
@@ -0,0 +1,53 @@
+package org.tools4j.fix.spec
+
+import spock.lang.Specification
+import org.tools4j.fix.spec.FixSpecDefinition.Message
+
+/**
+ * User: benjw
+ * Date: 9/3/2018
+ * Time: 6:37 AM
+ */
+class FixSpecParserTest extends Specification {
+    def "test FIX four four"() {
+        when:
+        final FixSpecDefinition fixSpec = new FixSpecParser('path/to/FIX44.xml').parseSpec()
+
+        then:
+        assert fixSpec.fieldsByName.size() == 916
+        assert fixSpec.groupsByName.size() == 52
+        assert fixSpec.messagesByName.size() == 92
+
+        then:
+        final Message nos = fixSpec.messagesByMsgType.get("D")
+        assert nos.fields.size() == 149
+    }
+
+    def "test FIX default"() {
+        when:
+        final FixSpecDefinition fixSpec = new FixSpecParser().parseSpec()
+
+        then:
+        assert fixSpec.fieldsByName.size() == 1606
+        assert fixSpec.groupsByName.size() == 117
+        assert fixSpec.messagesByName.size() == 117
+
+        then:
+        final Message nos = fixSpec.messagesByMsgType.get("D")
+        assert nos.fields.size() == 232
+    }
+
+    def "test FIX five zero SP two - from working dir"() {
+        when:
+        final FixSpecDefinition fixSpec = new FixSpecParser('fixgrep-core/src/main/resources/FIX50SP2.xml').parseSpec()
+
+        then:
+        assert fixSpec.fieldsByName.size() == 1606
+        assert fixSpec.groupsByName.size() == 117
+        assert fixSpec.messagesByName.size() == 117
+
+        then:
+        final Message nos = fixSpec.messagesByMsgType.get("D")
+        assert nos.fields.size() == 232
+    }
+}
