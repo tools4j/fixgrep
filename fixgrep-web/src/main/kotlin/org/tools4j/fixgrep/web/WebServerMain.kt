a7477634134efc3830a0211407c4f121853f91ce
ben <bjwarner@gmail.com>
2018-12-03 17:36:50 +0000

    First commit of web stuff

---
+++ fixgrep-web/src/main/kotlin/org/tools4j/fixgrep/web/WebServerMain.kt
@@ -0,0 +1,25 @@
+package org.tools4j.fixgrep.web
+
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+
+/**
+ * User: benjw
+ * Date: 03/12/2018
+ * Time: 17:08
+ */
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("My Example Blog", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+}
